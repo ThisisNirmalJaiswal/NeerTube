32937b2a0ee69f4df96bca6d95b5072751640070
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-03-16 00:24:57 +0530

    final push

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+        maxResults: 50,
+    },
+    headers: {
+        'X-RapidAPI-Key':"3e86fe9a7dmsha82b00d70c740f4p14fab6jsn37558408b88d",
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+};
